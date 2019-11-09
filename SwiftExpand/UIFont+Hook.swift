03c06ea8e9e46dbe0c3e1bf7a6644332e067cd3e
Shang <shang1219178163@gmail.com>
2019-11-09 17:23:38 +0800

    update

--- SwiftExpand/UIFont+Hook.swift
+++ SwiftExpand/UIFont+Hook.swift
@@ -6,35 +6,37 @@
 //  Copyright © 2019 BN. All rights reserved.
 //
 
-let kPingFang           = "PingFang SC";
-let kPingFangMedium     = "PingFangSC-Medium";
-let kPingFangSemibold   = "PingFangSC-Semibold";
-let kPingFangLight      = "PingFangSC-Light";
-let kPingFangUltralight = "PingFangSC-Ultralight";
-let kPingFangRegular    = "PingFangSC-Regular";
-let kPingFangThin       = "PingFangSC-Thin";
-
-
 import UIKit
 
-@objc extension UIFont{
-    override public class func initializeMethod() {
+@objc public extension UIFont{
+    override class func initializeMethod() {
         super.initializeMethod();
         if self == UIFont.self {
             let onceToken = "Method Swizzling_\(NSStringFromClass(classForCoder()))";
             //DispatchQueue函数保证代码只被执行一次，防止又被交换回去导致得不到想要的效果
             DispatchQueue.once(token: onceToken) {
                 let oriSel0 = #selector(systemFont(ofSize:))
-                let repSel0 = #selector(hook_systemFont(ofSize:))
+                let repSel0 = #selector(swz_systemFont(ofSize:))
                 
-                _ = swizzleMethodInstance(UIImageView.self, origSel: oriSel0, replSel: repSel0);
+                let _ = swizzleMethodInstance(UIImageView.self, origSel: oriSel0, replSel: repSel0);
                 
             }
         }
     }
     
-    private class func hook_systemFont(ofSize fontSize: CGFloat) -> UIFont{
-        return UIFont(name: kPingFangRegular, size: fontSize)!
+    private class func swz_systemFont(ofSize fontSize: CGFloat) -> UIFont{
+        return UIFont(name: UIFont.kPingFangRegular, size: fontSize)!
     }
+
+}
+
+@objc extension UIFont{
+    static let kPingFang           = "PingFang SC";
+    static let kPingFangMedium     = "PingFangSC-Medium";
+    static let kPingFangSemibold   = "PingFangSC-Semibold";
+    static let kPingFangLight      = "PingFangSC-Light";
+    static let kPingFangUltralight = "PingFangSC-Ultralight";
+    static let kPingFangRegular    = "PingFangSC-Regular";
+    static let kPingFangThin       = "PingFangSC-Thin";
 
 }
