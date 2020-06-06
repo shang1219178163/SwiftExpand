58cbbe0c2f499cbbf8e4394a9776260c6b804f78
Shang <shang1219178163@gmail.com>
2020-06-06 14:29:09 +0800

    * * 测试 222

--- Shells/git_action.sh
+++ Shells/git_action.sh
@@ -95,7 +95,7 @@ updatePod(){
 #    git push -u origin master || exit 1
     git push || exit 1
 
-    echo_green "--- Step: add tag to local reposit ---"
+    echo_green "--- Step: add tag to local reposit ：`git log -1 --pretty=format:"%s" --graph`---"
     git tag -a ${version} -m "`git log -1 --pretty=format:"%s" --graph`" || exit 1
 
     echo_green "--- Step: push tag to remote reposit ---"
