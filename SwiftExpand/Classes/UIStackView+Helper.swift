08919ec56f69a65d11dba5257377456b951fe6f7
Shang <shang1219178163@gmail.com>
2020-11-17 18:32:47 +0800

    优化

--- SwiftExpand/Classes/UIStackView+Helper.swift
+++ SwiftExpand/Classes/UIStackView+Helper.swift
@@ -12,14 +12,14 @@ import UIKit
 @objc public extension UIStackView {
     
     static func create(_ rect: CGRect, spacing: CGFloat = 10.0) -> Self {
-        let view: UIStackView = self.init(frame: rect);
+        let view = self.init(frame: rect);
 //        view.autoresizingMask = [.width, .height];
         //设置子视图间隔
         view.spacing = spacing
         //子视图的高度或宽度保持一致
 //        view.distribution = .fillEqually
         view.distribution = .fillProportionally
-        return view as! Self;
+        return view
     }
 
     /// 设置子视图显示比例(此方法前请设置 .axis/.orientation)
