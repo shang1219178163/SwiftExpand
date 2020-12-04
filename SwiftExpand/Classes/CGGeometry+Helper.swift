1b573546cee2299258e40d7482d89cb66f7ad7d6
Shang <shang1219178163@gmail.com>
2020-12-04 20:56:30 +0800

    新增数值尺寸

---
+++ SwiftExpand/Classes/CGGeometry+Helper.swift
@@ -0,0 +1,23 @@
+//
+//  CGGeometry+Helper.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/12/4.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+
+public extension CGRect {
+
+}
+
+public extension UIEdgeInsets{
+    
+    static func + (_ lhs: UIEdgeInsets, _ rhs: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsets(top: lhs.top + rhs.top,
+                            left: lhs.left + rhs.left,
+                            bottom: lhs.bottom + rhs.bottom,
+                            right: lhs.right + rhs.right)
+    }
+}
