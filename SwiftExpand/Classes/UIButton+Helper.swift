130eeae8a97d3b649e6a49a88c8d2e817d5c60de
Shang <shang1219178163@gmail.com>
2020-12-19 14:33:10 +0800

    方法重写

--- SwiftExpand/Classes/UIButton+Helper.swift
+++ SwiftExpand/Classes/UIButton+Helper.swift
@@ -12,6 +12,23 @@ import UIKit
 
 
 @objc public extension UIButton{
+    
+    private struct AssociateKeys {
+        static var closure   = "UIButton" + "closure"
+    }
+    /// UIControl 添加回调方式
+    override func addActionHandler(_ action: @escaping ((UIButton) ->Void), for controlEvents: UIControl.Event = .touchUpInside) {
+        addTarget(self, action:#selector(p_handleAction(_:)), for:controlEvents);
+        objc_setAssociatedObject(self, &AssociateKeys.closure, action, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    /// 点击回调
+    private func p_handleAction(_ sender: UIButton) {
+        if let block = objc_getAssociatedObject(self, &AssociateKeys.closure) as? ((UIButton) ->Void) {
+            block(sender);
+        }
+    }
+    
     ///自定义按钮类型
     @objc enum CustomType: Int {
              ///白底黑色
--- SwiftExpand/Classes/UISegmentedControl+Helper.swift
+++ SwiftExpand/Classes/UISegmentedControl+Helper.swift
@@ -10,6 +10,19 @@ import UIKit
 @objc public extension UISegmentedControl{
     private struct AssociateKeys {
         static var items   = "UISegmentedControl" + "items"
+        static var closure = "UISegmentedControl" + "closure"
+    }
+    /// UIControl 添加回调方式
+    override func addActionHandler(_ action: @escaping ((UISegmentedControl) ->Void), for controlEvents: UIControl.Event = .valueChanged) {
+        addTarget(self, action:#selector(p_handleAction(_:)), for:controlEvents);
+        objc_setAssociatedObject(self, &AssociateKeys.closure, action, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
+    /// 点击回调
+    private func p_handleAction(_ sender: UISegmentedControl) {
+        if let block = objc_getAssociatedObject(self, &AssociateKeys.closure) as? ((UISegmentedControl) ->Void) {
+            block(sender);
+        }
     }
     
     var currentTitle: String? {
