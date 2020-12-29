46ce1c7fa71903bbca177b35cb6a0d45e963a66f
Shang <shang1219178163@gmail.com>
2020-12-29 18:07:15 +0800

    UserDefaults 下标方法重构

--- SwiftExpand/Classes/UserDefaults+Helper.swift
+++ SwiftExpand/Classes/UserDefaults+Helper.swift
@@ -12,40 +12,12 @@ import Foundation
     
     subscript(key: String) -> Any? {
         get {
-            return object(forKey: key)
+            return value(forKey: key)
         }
         set {
             set(newValue, forKey: key)
         }
     }
-    
-    /// UserDefaults 二次封装
-//    static func setObject(_ object: Any?, forKey key: String) {
-//        standard.setValue(object, forKey: key)
-//    }
-//    /// UserDefaults 二次封装
-//    static func object(forKey key: String, default defaultProvider: Any) -> Any? {
-//        return standard.value(forKey: key) ?? defaultProvider
-//    }
-//    /// UserDefaults 二次封装
-//    static func object(forKeyPath keyPath: String, default defaultProvider: Any) -> Any? {
-//        return standard.value(forKeyPath: keyPath) ?? defaultProvider
-//    }
-//    /// UserDefaults 二次封装
-//    static func synchronize() {
-//         standard.synchronize()
-//     }
-//    ///UserDefaults 保存模型
-//    static func setArcObject(_ value: Any?, forkey defaultName: String) {
-//        guard let value = value else { return }
-//        let data = NSKeyedArchiver.archivedData(withRootObject: value)
-//        standard.setValue(data, forKey: defaultName)
-//    }
-//    ///UserDefaults 解包模型
-//    static func arcObject(forkey defaultName: String) -> Any? {
-//        guard let value = standard.object(forKey: defaultName) as? Data else { return nil}
-//        return NSKeyedUnarchiver.unarchiveObject(with: value);
-//    }
     
     ///UserDefaults 保存模型
     static func arcObject(_ value: Any?, forkey defaultName: String) {
@@ -82,16 +54,16 @@ import Foundation
 }
 
 public extension UserDefaults{
-    /// defaultProvider 默认值
-    subscript<T>(key: String, default defaultProvider: @autoclosure () -> T) -> T {
+    
+    subscript<T>(key: String) -> T? {
         get {
-            return value(forKey: key) as? T ?? defaultProvider()
+            return value(forKey: key) as? T
         }
         set {
             set(newValue, forKey: key)
         }
     }
-
+    
     /// 枚举默认支持 RawRepresentable
     subscript<T: RawRepresentable>(key: String) -> T? {
         get {
@@ -104,5 +76,99 @@ public extension UserDefaults{
             set(newValue?.rawValue, forKey: key)
         }
     }
+    
+    ///类属性下标方法
+    static subscript<T>(key: String) -> T? {
+        get {
+            return standard.value(forKey: key) as? T
+        }
+        set {
+            standard.set(newValue, forKey: key)
+        }
+    }
+    
+    /// 类属性下标方法(枚举默认支持 RawRepresentable)
+    static subscript<T: RawRepresentable>(key: String) -> T? {
+        get {
+            if let rawValue = standard.object(forKey: key) as? T.RawValue {
+                return T(rawValue: rawValue)
+            }
+            return nil
+        }
+        set {
+            standard.set(newValue?.rawValue, forKey: key)
+        }
+    }
+}
+
+
+public enum AppTheme: Int {
+    case light
+    case dark
+}
+
+public class SettingsService {
+    
+    public required init() {
+        
+    }
+    
+    public var isNotificationsEnabled: Bool {
+        get {
+            let isEnabled = UserDefaults.standard[#function] as? Bool
+            return isEnabled ?? true
+        }
+        set {
+//            DDLog(#function, String(describing: self), type(of: self), Self.self)
+            UserDefaults.standard.setValue(newValue, forKey: #function)
+        }
+    }
+    
+    public var appTheme: AppTheme {
+        get {
+            return UserDefaults.standard[#function] ?? .light
+        }
+        set {
+            UserDefaults.standard[#function] = newValue
+        }
+    }
+    
+    public static var isNotificationsEnabled: Bool {
+        get {
+            let isEnabled = UserDefaults.standard[#function] as? Bool
+            return isEnabled ?? true
+        }
+        set {
+//            DDLog(#function, String(describing: self), type(of: self), Self.self)
+            UserDefaults.standard.setValue(newValue, forKey: #function)
+        }
+    }
+    
+    public static var appTheme: AppTheme {
+        get {
+            return UserDefaults.standard[#function] ?? .light
+        }
+        set {
+            UserDefaults.standard[#function] = newValue
+        }
+    }
+    
+//    public func testFunc() {
+//        DDLog(#function, String(describing: self), type(of: self), Self.self)
+//    }
+//
+//    public static func testFunc() {
+//        DDLog(#function, String(describing: self), type(of: self), Self.self)
+//    }
+//
+//    public func test() {
+//        let service = SettingsService()
+//        service.isNotificationsEnabled = false
+//        SettingsService.isNotificationsEnabled = false
+//
+//        DDLog(service.isNotificationsEnabled, SettingsService.isNotificationsEnabled)
+//        service.testFunc()
+//        SettingsService.testFunc()
+//    }
 }
 
