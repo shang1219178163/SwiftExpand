ab1d255062939adf18573e4194a5462c91e81b2d
Shang <shang1219178163@gmail.com>
2021-05-18 19:14:25 +0800

    代码优化

--- SwiftExpand/Classes/AVAudioSession+Helper.swift
+++ SwiftExpand/Classes/AVAudioSession+Helper.swift
@@ -12,15 +12,11 @@ import AVFoundation
 @objc public extension AVAudioSession {
 
     ///音量开关
-    static func volume(_ soundoff: Bool) {
+    func volume(_ soundoff: Bool) throws {
         if #available(iOS 10.0, *) {
             let category: AVAudioSession.Category = soundoff == true ? .record : .ambient;
-            do {
-                try AVAudioSession.sharedInstance().setCategory(category, mode: .default)
-                try AVAudioSession.sharedInstance().setActive(true)
-            } catch {
-                print(error)
-            }
+            try setCategory(category, mode: .default)
+            try setActive(true)
         }
     }
 }
