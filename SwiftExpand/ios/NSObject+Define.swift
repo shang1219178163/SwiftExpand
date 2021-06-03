51f742b7131d168a75e1e4240c82aef538ba9e65
Shang <shang1219178163@gmail.com>
2021-06-03 16:11:32 +0800

    新功能扩展

--- SwiftExpand/ios/NSObject+Define.swift
+++ SwiftExpand/ios/NSObject+Define.swift
@@ -61,3 +61,169 @@ public func UICtrFromString(_ vcName: String) -> UIViewController {
     controller.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     return controller;
 }
+
+
+
+@objc public extension NSObject{
+    static var named: String {
+        let array = NSStringFromClass(self).components(separatedBy: ".")
+        return array.last ?? ""
+    }
+
+    ///遍历成员变量列表
+    func enumerateIvars(_ block: @escaping ((Ivar, String, Any?)->Void)) {
+        var count: UInt32 = 0
+        guard let list = class_copyIvarList(self.classForCoder, &count) else { return }
+        defer {
+            free(list)// 释放c语言对象
+        }
+
+        for i in 0..<Int(count) {
+            let ivar = list[i]
+            //转换成String字符串
+            guard let name = ivar_getName(ivar),
+                let strName = String(cString: name, encoding: String.Encoding.utf8) else {
+                //继续下一次遍历
+                continue
+            }
+            //利用kvc 取值
+            let value = self.value(forKey: strName)
+            block(ivar, strName, value)
+        }
+    }
+    ///遍历属性列表
+    func enumeratePropertys(_ block: @escaping ((objc_property_t, String, Any?)->Void)) {
+        var count: UInt32 = 0
+        guard let list = class_copyPropertyList(self.classForCoder, &count) else { return }
+        defer {
+            free(list)// 释放c语言对象
+        }
+        
+        for i in 0..<Int(count) {
+            let property: objc_property_t = list[i]
+            //获取成员变量的名称 -> c语言字符串
+            let name = property_getName(property)
+            //转换成String字符串
+            guard let strName = String(cString: name, encoding: .utf8) else {
+                //继续下一次遍历
+                continue
+            }
+            //利用kvc 取值
+            let value = self.value(forKey: strName)
+            block(property, strName, value)
+        }
+    }
+    ///遍历方法列表
+    func enumerateMethods(_ block: @escaping ((Method, String)->Void)) {
+        var count: UInt32 = 0
+        guard let list = class_copyMethodList(self.classForCoder, &count) else { return }
+        defer {
+            free(list)// 释放c语言对象
+        }
+
+        for i in 0..<Int(count) {
+            let method: Method = list[i]
+            //获取成员变量的名称 -> c语言字符串
+            let name: Selector = method_getName(method)
+            //转换成String字符串
+            let strName = NSStringFromSelector(name)
+            block(method, strName)
+        }
+    }
+    ///遍历遵循的协议列表
+    func enumerateProtocols(_ block: @escaping ((Protocol, String)->Void)) {
+        var count: UInt32 = 0
+        guard let list = class_copyProtocolList(self.classForCoder, &count) else { return }
+
+        for i in 0..<Int(count) {
+            let proto: Protocol = list[i]
+            //获取成员变量的名称 -> c语言字符串
+            let name = protocol_getName(proto)
+            //转换成String字符串
+            guard let strName = String(cString: name, encoding: String.Encoding.utf8) else {
+                //继续下一次遍历
+                continue
+            }
+            block(proto, strName)
+        }
+    }
+
+    /// 模型自动编码
+    func se_encode(with aCoder: NSCoder) {
+        enumeratePropertys { (property, name, value) in
+            guard let value = self.value(forKey: name) else { return }
+            //进行编码
+            aCoder.encode(value, forKey: name)
+        }
+    }
+    
+    /// 模型自动解码
+    func se_decode(with aDecoder: NSCoder) {
+        enumeratePropertys { (property, name, value) in
+            //进行解档取值
+            guard let value = aDecoder.decodeObject(forKey: name) else { return }
+            //利用kvc给属性赋值
+            self.setValue(value, forKeyPath: name)
+        }
+    }
+    /// 字典转模型
+//    convenience init(dic: [String: Any]) {
+//        self.init();
+//        self.setValuesForKeys(dic)
+//    }
+//    ///详情模型转字典(不支持嵌套)
+//    func toDictionary() -> [AnyHashable : Any] {
+//        var dic: [AnyHashable : Any] = [:]
+//        self.enumeratePropertys { (property, name, value) in
+//            dic[name] = value ?? ""
+//        }
+//        return dic
+//    }
+    
+    func synchronized(_ lock: AnyObject, block: () -> Void) {
+        objc_sync_enter(lock)
+        block()
+        objc_sync_exit(lock)
+    }
+            
+    // MARK: - KVC
+
+    /// 返回key对应的值
+    func valueText(forKey key: String, defalut: String = "-") -> String{
+        if key == "" {
+            return "";
+        }
+        if let result = self.value(forKey: key) {
+            return "\(result)" != "" ? "\(result)" : defalut;
+        }
+        return defalut;
+    }
+    /// 返回key对应的值
+    func valueText(forKeyPath keyPath: String, defalut: String = "-") -> String{
+        if keyPath == "" {
+            return "";
+        }
+        if let result = self.value(forKeyPath: keyPath) {
+            return "\(result)" != "" ? "\(result)" : defalut;
+        }
+        return defalut;
+    }
+
+}
+
+
+public extension NSObjectProtocol where Self: NSObject {
+    /// viewModel.observe(\.playButtonTitle) { [playButton] in playButton!.setTitle($0, for: .normal) },
+    func observe<Value>(_ keyPath: KeyPath<Self, Value>, onChange: @escaping (Value) -> ()) -> NSKeyValueObservation {
+        return observe(keyPath, options: [.initial, .new]) { _, change in
+            guard let newValue = change.newValue else { return }
+            onChange(newValue)
+        }
+    }
+    
+    ///viewModel.bind(\.navigationTitle, to: navigationItem, at: \.title)
+    func bind<Value, Target>(_ sourceKeyPath: KeyPath<Self, Value>, to target: Target, at targetKeyPath: ReferenceWritableKeyPath<Target, Value>) -> NSKeyValueObservation {
+        return observe(sourceKeyPath) { target[keyPath: targetKeyPath] = $0 }
+    }
+
+}
