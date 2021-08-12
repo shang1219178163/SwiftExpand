09e2dbc86e6712c1d9b3d6fd0935ad9fa85a942b
Shang <shang1219178163@gmail.com>
2021-08-12 13:47:50 +0800

    富文本方法重构

--- SwiftExpand/Classes/NSAttributedString+Helper.swift
+++ SwiftExpand/Classes/NSAttributedString+Helper.swift
@@ -58,7 +58,7 @@ import Foundation
     }
     
     /// 创建超链接富文本
-    static func createLink(_ text: String, linkDic: [String: String], font: Font) -> NSMutableAttributedString {
+    static func createLink(_ text: String, dic: [String: String], font: Font) -> NSMutableAttributedString {
         let attDic: [NSAttributedString.Key: Any] = [
             .font: font as Any
         ]
