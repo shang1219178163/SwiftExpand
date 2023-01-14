608d44943205ee9b43e51984e3f84f10ed20bb41
Shang <shang1219178163@gmail.com>
2023-01-14 17:38:10 +0800

    更新

--- SwiftExpand/Classes/JSONSerialization+Helper.swift
+++ SwiftExpand/Classes/JSONSerialization+Helper.swift
@@ -22,14 +22,3 @@ import Foundation
         return obj
     }
 }
-
-extension Encodable {
-    
-    var asDictionary: [String: Any] {
-        guard let data = try? JSONEncoder().encode(self) else { return [:] }
-        guard let dictionary = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
-            return [:]
-        }
-        return dictionary
-    }
-}
